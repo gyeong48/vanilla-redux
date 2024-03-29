bbfd0d614f1a25f6a1dbeb8e177997ec446774dc
gyeong48 <seogyeonghwan48@gmail.com>
2024-03-29 12:49:54 +0900

    refactor reduxtoolkit todoList

--- src/store.js
+++ src/store.js
@@ -1,38 +1,25 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+//redux toolkit은 state를 직접 변경해도 된다.
+//createReducer를 사용할 때 두가지 선택지가 있다. state를 변경 하던지, 새로운 상태를 반환하던지
+//여기서 중요한 부분은 변경을 할 때는 반환을 하면 안된다. 그리고 반환을 하려면 꼭 새로운 상태의 state를 반환해야하 한다.
+//redux toolkit이 내부에서 순수 redux를 사용할 때 처럼 코드를 실행해준다.
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    });
+});
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((todo) => todo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-//순수 리덕스를 사용할 때 우리가 직접 렌더링을 해주었다.
-//리액트는 변화되는 부분만 리렌더링 한다. 그래서 리액트의 기술과 함께 리덕스를 사용할 수 있도록
-//react-redux 라이브러리를 사용한다.
+const store = createStore(reducer, composeWithDevTools());
 store.subscribe(() => {});
 
 export const actionCreators = {
